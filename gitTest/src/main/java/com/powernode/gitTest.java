87c8f9f00212f8e95e8cca8e72a4e897bbdd3916
jiasheng-l <140054385+jiasheng-l@users.noreply.github.com>
2023-07-20 19:32:25 +0800

    pull commit

--- gitTest/src/main/java/com/powernode/gitTest.java
+++ gitTest/src/main/java/com/powernode/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
